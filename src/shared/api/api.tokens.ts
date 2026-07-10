33233840df044de9835192da00a0e1786ebd28db
Yaroslav <lordrom92@gmail.com>
2026-07-10 09:58:43 +0300

    asd

--- src/shared/api/api.tokens.ts
+++ src/shared/api/api.tokens.ts
@@ -1,7 +1,7 @@
 import { InjectionToken } from '@angular/core';
 
-export const API_BASE_URL = new InjectionToken('API_BASE_URL');
+export const API_BASE_URL = new InjectionToken<string>('API_BASE_URL');
 
-export const AUTH_API_URL = new InjectionToken('AUTH_API_URL');
+export const AUTH_API_URL = new InjectionToken<string>('AUTH_API_URL');
 
-export const USER_API_URL = new InjectionToken('USER_API_URL');
+export const USER_API_URL = new InjectionToken<string>('USER_API_URL');
